47ad76669f254f6412684392e77ef41e23194a42
Samantha Fernandes <samfer2359@gmail.com>
2026-06-24 18:16:10 +0530

    google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Dinosaur from "./pages/Dinosaur"
-import DinosaurDetail from "./pages/DinosaurDetail"
-import About from "./pages/About"
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
+
+import Home from "./pages/Home";
+import Dinosaur from "./pages/Dinosaur";
+import DinosaurDetail from "./pages/DinosaurDetail";
+import About from "./pages/About";
+
+import Navbar from "./components/Navbar";
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname,
+    });
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <AnalyticsTracker />
+
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dinosaur" element={<Dinosaur />} />
@@ -16,7 +36,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-QEFXC60WXD");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
